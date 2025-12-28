a7912b44321aea2f14c91927b6b0adfa1f89f3a7
Chris Mann <chris@lesgtrandsvoisins.com>
2025-12-28 10:19:14 +0100

    Code Cleanup

--- derivations/generate-self-certs/selfcerts.sh
+++ derivations/generate-self-certs/selfcerts.sh
@@ -1,1 +1,2 @@
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/C=FR/ST=IdF/L=Paris/O=Les Grands Voisins/OU=WhoWhatEtc./CN=whowhatetc.com"
+#!/bin/sh
+openssl req -x509 -newkey rsa:4096 -keyout /tmp/key.pem -out /tmp/cert.pem -sha256 -days 3650 -nodes -subj "/C=FR/ST=IdF/L=Paris/O=Les Grands Voisins/OU=WhoWhatEtc./CN=whowhatetc.com"
--- scripts/selfcerts.sh
+++
@@ -1,1 +0,0 @@
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/C=FR/ST=IdF/L=Paris/O=Les Grands Voisins/OU=WhoWhatEtc./CN=whowhatetc.com"
