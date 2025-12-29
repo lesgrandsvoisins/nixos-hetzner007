692274a0306ea2df8424a59ab33b2534621a01e8
Chris Mann <chris@lesgtrandsvoisins.com>
2025-12-29 11:00:34 +0100

    flake update

--- containers/homarr/systemd-services.nix
+++ containers/homarr/systemd-services.nix
@@ -6,7 +6,6 @@ in {
       wantedBy = ["default.target"];
       description = "Système de tableaux de bords Homarr";
       script = "/home/homarr/homarr/start.sh";
-      environment = "/etc/homarr/homarr.env";
       serviceConfig = {
         WorkingDirectory = "/home/homarr/homarr";
         User = "homarr";
