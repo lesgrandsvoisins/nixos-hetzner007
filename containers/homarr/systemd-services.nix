922aedc8fe78895b5002dc75ef342c20960fad69
Chris Mann <chris@lesgtrandsvoisins.com>
2026-01-13 10:28:27 +0100

    Homarr Debug

--- containers/homarr/systemd-services.nix
+++ containers/homarr/systemd-services.nix
@@ -12,9 +12,9 @@ in {
       wantedBy = ["default.target"];
       description = "Système de tableaux de bords Homarr";
       script = ''
-        node --env-file=/etc/homarr/homarr.env apps/tasks/tasks.cjs &
-        node --env-file=/etc/homarr/homarr.env apps/websocket/wssServer.cjs &
-        pnpm -F nextjs start
+        /run/current-system/sw/bin/node --env-file=/etc/homarr/homarr.env apps/tasks/tasks.cjs &
+        /run/current-system/sw/bin/node --env-file=/etc/homarr/homarr.env apps/websocket/wssServer.cjs &
+        /run/current-system/sw/bin/pnpm -F nextjs start
       '';
       environment = {
         PATH = "/run/wrappers/bin:/home/homarr/.nix-profile/bin:/nix/profile/bin:/home/homarr/.local/state/nix/profile/bin:/etc/profiles/per-user/homarr/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin";
