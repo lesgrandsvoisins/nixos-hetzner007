786c38d06eec1e9944f5e46222ea7fe674f60202
Chris Mann <chris@mann.fr>
2026-01-18 19:59:06 +0100

    Missing certs

--- modules/security.nix
+++ modules/security.nix
@@ -28,6 +28,15 @@ in {
           extraDomainNames = ["www.gdvoisins.com"];
           group = "users";
         };
+        "keycloak.grandsvoisins.org" = {
+          dnsProvider = "clouddns";
+          credentialFiles = {
+            "CLOUDNS_AUTH_ID_FILE" = "/etc/.secrets/.cloudns.auth.id";
+            "CLOUDNS_AUTH_PASSWORD_FILE" = "/etc/.secrets/.cloudns.auth.password";
+            "CLOUDNS_AUTH_EMAIL_FILE" = "/etc/.secrets/.cloudns.auth.email";
+          };
+          group = "users";
+        };
         "ggvv.org" = {
           dnsProvider = "clouddns";
           # environmentFile = "/etc/.secrets/.cloudns.auth";
