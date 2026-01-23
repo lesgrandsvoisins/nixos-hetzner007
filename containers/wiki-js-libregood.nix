8faee8807925fd7b7b4be6e4184253550da5083b
Chris Mann <chris@mann.fr>
2026-01-23 20:17:22 +0100

    www.libregood.com

--- containers/wiki-js-libregood.nix
+++ containers/wiki-js-libregood.nix
@@ -4,9 +4,11 @@
   lib,
   vars,
   ...
-}: let
+}:
+let
   # vars = import ../vars.nix;
-in {
+in
+{
   networking.hosts = {
     "10.0.12.102" = [ "wiki-js-libregood.local" ];
     "fa12::102" = [ "wiki-js-libregood.local" ];
@@ -39,60 +41,64 @@ in {
     config = {
       system.stateVersion = "25.11";
 
-  users.users.wiki-js = {
-    isSystemUser = true;
-    uid = vars.uid.wiki-js;
-    group = "services";
-  };
-  users.groups.services.gid = vars.gid.services;
-
-  systemd.services.wiki-js.serviceConfig.User = "wiki-js";
-  systemd.services.wiki-js.serviceConfig.Group = "services";
-  systemd.tmpfiles.rules = [
-    "d /etc/wiki-js 0750 wiki-js services"
-    "f /etc/wiki-js/.env 0640 wiki-js services"
-    # "L /run/postgresql/.s.PGSQL.5434 /run/postgresql/.s.PGSQL.5432"
-  ];
-  services.cron.systemCronJobs = ["0 0 1 * *  root systemctl restart wiki-js"];
-
-  services.wiki-js = {
-    enable = true;
-    environmentFile = "/etc/wiki-js/.env";
-    settings = {
-      port = vars.ports.wiki-js-libregood-http;
-      bindIP = vars.ips.wiki-js;
-      db = {
-        # host = "2a01:4f8:241:4faa::10";
-        port = vars.ports.postgresql;
-        # host = "localhost";
-        host = "/run/postgresql/";
-        # host = "/run/postgresql/";
-        db = "wiki-js";
-        user = "wiki-js";
-        # ssl = true;
+      users.users.wiki-js = {
+        isSystemUser = true;
+        uid = vars.uid.wiki-js;
+        group = "services";
       };
-      loglevel = "info";
-      ssl = {
-        enabled = true;
-        port = vars.ports.wiki-js-libregood-https;
-        provider = "custom";
-        format = "pem";
-        cert = "/etc/wiki-js-libregood/wiki-js-libregood.local.pem";
-        key = "/etc/wiki-js-libregood/wiki-js-libregood.local-key.pem";
-        passphrase = null;
-        dhparam = null;
+      users.groups.services.gid = vars.gid.services;
+
+      systemd.services.wiki-js.serviceConfig.User = "wiki-js";
+      systemd.services.wiki-js.serviceConfig.Group = "services";
+      systemd.tmpfiles.rules = [
+        "d /etc/wiki-js 0750 wiki-js services"
+        "f /etc/wiki-js/.env 0640 wiki-js services"
+        # "L /run/postgresql/.s.PGSQL.5434 /run/postgresql/.s.PGSQL.5432"
+      ];
+      services.cron.systemCronJobs = [ "0 0 1 * *  root systemctl restart wiki-js" ];
+
+      services.wiki-js = {
+        enable = true;
+        environmentFile = "/etc/wiki-js/.env";
+        settings = {
+          port = vars.ports.wiki-js-libregood-http;
+          bindIP = vars.ips.wiki-js;
+          db = {
+            # host = "2a01:4f8:241:4faa::10";
+            port = vars.ports.postgresql;
+            # host = "localhost";
+            host = "/run/postgresql/";
+            # host = "/run/postgresql/";
+            db = "wiki-js";
+            user = "wiki-js";
+            # ssl = true;
+          };
+          loglevel = "info";
+          ssl = {
+            enabled = true;
+            port = vars.ports.wiki-js-libregood-https;
+            provider = "custom";
+            format = "pem";
+            cert = "/etc/wiki-js-libregood/wiki-js-libregood.local.pem";
+            key = "/etc/wiki-js-libregood/wiki-js-libregood.local-key.pem";
+            passphrase = null;
+            dhparam = null;
+          };
+        };
       };
-    };
-  };
-  services.postgresql = {
-    ensureUsers = [
-      {
-        name = "wiki-js";
-        ensureDBOwnership = true;
-      }
-    ];
-    ensureDatabases = ["wiki-js"];
-  };
+      services.postgresql = {
+        enable = true;
+        initialScript = pkgs.writeText "init-sql-script" ''
+          GRANT ALL PRIVILEGES ON DATABASE wiki-js-libregood to wiki-js;
+        '';
+        ensureUsers = [
+          {
+            name = "wiki-js";
+            # ensureDBOwnership = true;
+          }
+        ];
+        ensureDatabases = [ "wiki-js-libregood"  ];
+      };
     };
   };
 }
