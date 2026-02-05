8f14250958e84a7abfe6296f2ba88e91f0878c98
Chris Mann <chris@mann.fr>
2026-02-05 14:19:44 +0100

    postgresql backup

--- modules/services/vaultwarden.nix
+++ modules/services/vaultwarden.nix
@@ -4,8 +4,10 @@
   config,
   vars,
   ...
-}: let
-in {
+}:
+let
+in
+{
   systemd.tmpfiles.rules = [
     "d /etc/vaultwarden 0755 vaultwarden services"
     "f /etc/vaultwarden/vaultwarden.env 0600 vaultwarden services"
@@ -13,7 +15,11 @@ in {
   networking.hosts = {
     "127.0.0.1" = [ "vaultwarden.local" ];
   };
-  users.users.vaultwarden.extraGroups = ["services"];
+  users.users.vaultwarden.extraGroups = [ "services" ];
+  services.postgresqlBackup = {
+    enable = true;
+    backupAll = true;
+  };
   services.vaultwarden = {
     domain = "vw.gv.je";
     enable = true;
@@ -24,20 +30,22 @@ in {
     dbBackend = "postgresql";
     config = {
       SIGNUPS_ALLOWED = true;
-      SMTP_HOST="mail.lesgrandsvoisins.com";
-      SMTP_FROM="list@lesgrandsvoisins.com";
-      SMTP_FROM_NAME="Les Grands Voisins (List / Vaultwarden)";
+      SMTP_HOST = "mail.lesgrandsvoisins.com";
+      SMTP_FROM = "list@lesgrandsvoisins.com";
+      SMTP_FROM_NAME = "Les Grands Voisins (List / Vaultwarden)";
       # SMTP_USERNAME="";
       # SMTP_PASSWORD="";
-      SMTP_TIMEOUT=15;
-      ROCKET_PORT=8222;
-      # ROCKET_ADDRESS = "127.0.0.1";
-      ROCKET_ADDRESS = "vaultwarden.local";
-      # ROCKET_LOG = "critical";  
+      SMTP_TIMEOUT = 15;
+      ROCKET_PORT = 8222;
+      ROCKET_ADDRESS = "127.0.0.1";
+      # ROCKET_ADDRESS = "vaultwarden.local";
+      # ROCKET_LOG = "critical";
       SMTP_PORT = 465;
       # SMTP_SSL = true;
       SMTP_SECURITY = "force_tls";
-      ROCKET_TLS="{certs=\"/etc/vaultwarden/.tls/vaultwarden.local.pem\",key=\"/etc/vaultwarden/.tls/vaultwarden.local-key.pem\"}";
+      # ROCKET_TLS.certs = "/etc/vaultwarden/.tls/vaultwarden.local.pem"; # in env
+      # ROCKET_TLS.key = "/etc/vaultwarden/.tls/vaultwarden.local-key.pem";  # in envà
+      # IN ENV : ROCKET_TLS={certs="/etc/vaultwarden/.tls/vaultwarden.local.pem",key="/etc/vaultwarden/.tls/vaultwarden.local-key.pem"}
     };
   };
 }
