67a307ddfa2007b94971ca0aec6bf4cb65586ec7
Chris Mann <chris@lesgtrandsvoisins.com>
2026-02-22 19:45:52 +0100

    pre_login_hook chatgpt error

--- modules/services/sftpgo.nix
+++ modules/services/sftpgo.nix
@@ -54,10 +54,7 @@ in {
         # pre_login_hook
         # post_login_hook
         create_default_admin = true;
-        pre_login_hook = {
-          enabled = true;
-          hook = "${sftpgo-prelogin-hook}/bin/sftpgo-prelogin-hook";
-        };
+        pre_login_hook = "${sftpgo-prelogin-hook}/bin/sftpgo-prelogin-hook";
       };
       webdavd.bindings = [
         {
