eb8a32417deb8424ccb0fd3f218b36d9e677e441
Chris Mann <chris@lesgtrandsvoisins.com>
2026-02-22 09:28:43 +0100

    data_provider

--- modules/services/sftpgo.nix
+++ modules/services/sftpgo.nix
@@ -13,7 +13,6 @@ in {
   systemd.services.sftpgo.environment = {"SFTPGO_SMTP__PASSWORD" = "$(cat /etc/sftpgo/.secret.smtppassword)";};
   systemd.services.sftpgo.environment = {"SFTPGO_DEFAULT_ADMIN_USERNAME" = "adminsftpgo";};
   systemd.services.sftpgo.environment = {"SFTPGO_DEFAULT_ADMIN_PASSWORD" = "$(cat /etc/sftpgo/.secret.adminpassword)";};
-  SFTPGO_DEFAULT_ADMIN_USERNAME
   systemd.tmpfiles.rules = [
     "f /etc/sftpgo/.secret.smtppassword 0660 sftpgo root"
     "f /etc/sftpgo/.secret.postgresqlpassword 0660 sftpgo root"
