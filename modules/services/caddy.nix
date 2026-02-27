b3676f7cd5968bce89e6806a31b93cc3771e4832
Chris Mann <chris@lesgtrandsvoisins.com>
2026-02-27 08:07:20 +0100

    cors on webdav.gv.je

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -12,7 +12,7 @@
   cors_any_gvje = ''
     # Validate multiple origins using regex
     @cors_origin_match {
-      header_regexp origin Origin ^https://[-A-z0-9]*\.)?gv\.je.*$
+      header_regexp origin Origin ^https://[-A-z0-9]*\.?gv\.je.*$
     }
 
     # Preflight for matched origins
@@ -204,7 +204,7 @@ in {
           file_server
           root * /var/www/keeweb
 
-          ${cors_any_gvje}
+          # ${cors_any_gvje}
         '';
       };
       "test.whowhatetc.com" = {
