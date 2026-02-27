9f53dda72e485f809694e38ffb4adc434e08ada4
Chris Mann <chris@lesgtrandsvoisins.com>
2026-02-27 12:43:49 +0100

    Vikunja

--- modules/services/vikunja.nix
+++ modules/services/vikunja.nix
@@ -32,7 +32,7 @@ in {
       database = {
         type = lib.mkForce "postgres";
         user = "vikunja";
-        host = lib.mkForce "/var/run/postgresql/.s.PGSQL.5434";
+        host = lib.mkForce "/var/run/postgresql:5434";
         # host = lib.mkForce "127.0.0.1:5434";
         database = "vikunja";
         password.file = "/etc/vikunja/.secrets/.vikunja@postgresql";
