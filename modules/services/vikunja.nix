30234f7c123ae2ef081ab89ad18bdb54c0920577
Chris Mann <chris@lesgtrandsvoisins.com>
2026-02-27 13:16:40 +0100

    vikunja

--- modules/services/vikunja.nix
+++ modules/services/vikunja.nix
@@ -77,7 +77,7 @@ in {
             lougouturl = "https://key.gv.je/realms/master/protocol/openid-connect/logout";
             clientid = "vikunja";
             # clientsecret = "KEYGVJE_VIKUNJA_CLIENT_SECRET";
-            # clientsecret = "$KEYGVJE_VIKUNJA_CLIENT_SECRET";
+            clientsecret = "$KEYGVJE_VIKUNJA_CLIENT_SECRET";
             # clientsecret.file = "/etc/vikunja/oidc_client_secret_keygvje";
             # usernamefallback = true;
             # emailfallback = true;
