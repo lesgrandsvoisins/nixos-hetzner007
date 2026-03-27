375b8893628a7c8556a5f76645debe8c8349ab42
Chris Mann <chris@lesgtrandsvoisins.com>
2026-03-27 18:56:40 +0100

    UI Change to key.gv.je

--- modules/services/keycloak/gv-keycloak-theme.nix
+++ modules/services/keycloak/gv-keycloak-theme.nix
@@ -16,29 +16,26 @@ pkgs.stdenv.mkDerivation {
   installPhase = pkgs.lib.strings.concatStrings [
     "runHook preInstall"
     "mkdir -p $out"
-    ''
-      cp -a login $out
+    "cp -a login $out"
+    # ''
+    #   mkdir -p "$out/login/templates"
+    #   mkdir -p "$out/login/resources"
 
+    #   themeJar="$(find ${pkgs.keycloak}/lib/lib/main -name 'org.keycloak.keycloak-themes-*.jar' | head -n 1)"
 
-          # mkdir -p "$out/login/templates"
-          # mkdir -p "$out/login/resources"
+    #   if [ -z "$themeJar" ]; then
+    #     echo "Could not find keycloak themes jar"
+    #     exit 1
+    #   fi
 
-          # themeJar="$(find ${pkgs.keycloak}/lib/lib/main -name 'org.keycloak.keycloak-themes-*.jar' | head -n 1)"
+    #   tmpdir="$(mktemp -d)"
+    #   unzip -q "$themeJar" 'theme/base/login/*' -d "$tmpdir"
 
-          # if [ -z "$themeJar" ]; then
-          #   echo "Could not find keycloak themes jar"
-          #   exit 1
-          # fi
+    #   cp "$tmpdir/theme/base/login/template.ftl" \
+    #     "$out/login/templates/template.ftl"
 
-          # tmpdir="$(mktemp -d)"
-          # unzip -q "$themeJar" 'theme/base/login/*' -d "$tmpdir"
-
-          # cp "$tmpdir/theme/base/login/template.ftl" \
-          #   "$out/login/templates/template.ftl"
-
-          # sed -i 's/<\/body>/<a href="https:\/\/www.gv.je" class="site-action-button" aria-label="Open action">\n<img src="https:\/\/public.gv.je\/static\/web\/gvbtn\/gv-logo-512x512.png" class="site-action-button-img">\n<\/a>\n<\/body>/' $out/login/templates/template.ftl
-
-          runHook postInstall
-    ''
+    #   sed -i 's/<\/body>/<a href="https:\/\/www.gv.je" class="site-action-button" aria-label="Open action">\n<img src="https:\/\/public.gv.je\/static\/web\/gvbtn\/gv-logo-512x512.png" class="site-action-button-img">\n<\/a>\n<\/body>/' $out/login/templates/template.ftl
+    # ''
+    "runHook postInstall"
   ];
 }
