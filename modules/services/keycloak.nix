2a9cb87672f2f9982b706073b0a7d77b79ecee0b
Chris Mann <chris@lesgtrandsvoisins.com>
2026-03-31 17:33:59 +0200

    Extra built keycloak providers

--- modules/services/keycloak.nix
+++ modules/services/keycloak.nix
@@ -51,7 +51,7 @@
 
     src = ./keycloak/gv-keycloak-provider; # folder next to this .nix file
 
-    mvnHash = lib.fakeHash;
+    mvnHash = "sha256-Kr4sk1IjaLoGIdU6CwgPcKhqRWCnZcm0uBvIr4Qam6Y=";
 
     installPhase = ''
       runHook preInstall
