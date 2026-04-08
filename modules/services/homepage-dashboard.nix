d9e80819f6d7d8eab4ae9cbdf59fad83ba05f689
Chris Mann <chris@lesgtrandsvoisins.com>
2026-04-08 11:02:26 +0200

    homepage-dashboard

--- modules/services/homepage-dashboard.nix
+++ modules/services/homepage-dashboard.nix
@@ -120,11 +120,45 @@ in {
     ];
     services = [
       {
+        "Votre compte @gv.je unifié" = [
+          {
+            "CREER & GERER votre compte unifié @GV.je" = {
+              href = "https://key.gv.je/realms/master/account";
+              description = "Keycloak pour la connexion unifiée : Serveur de connexion et déconnexion unifiées (SSO en OAuth2) pour plusieurs services GV.";
+              icon = "si-keycloak";
+            };
+          }
+          {
+            "SFTP casiers numériques individuels" = {
+              href = "https://sftpgo.gv.je/";
+              icon = "mdi-folder-key-network";
+              noticon = "sh-sftpgo";
+              description = "Un espace pour ses fichiers et pour les partager.";
+            };
+          }
+          {
+            "Favoris (Linkding)" = {
+              description = "Partager des fichiers et des dossiers";
+              href = "https://linkding.lesgrandsvoisins.com/";
+              icon = "mdi-paperclip";
+            };
+          }
+          {
+            "HedgeDoc carnets collaboratifs en markdown" = {
+              href = "https://mark.lesgrandsvoisins.com/auth/oauth2";
+              noticon = "https://mark.lesgrandsvoisins.com/banner/banner_vertical_color.svg";
+              icon = "si-hedgedoc";
+              description = "Un serveur de documents en format Markdown pouvant être modifiés par plusieurs personnes en même temps.";
+            };
+          }
+        ];
+      }
+      {
         "Accès ouverts" = [
           {
-            "Jisti pour les réunions visio et formation" = {
+            "VISIO : Passez nous voir en virtual" = {
               href = "https://meet.lesgrandsvoisins.com/";
-              description = "Un espace de rencontres visio qui tient vraiement la route.";
+              description = " Notre serveur Jitsi offre un espace de visioconférences qui tient vraiment la route. ";
               icon = "si-jitsi";
             };
           }
@@ -204,40 +238,6 @@ in {
                   target = "{url}";
                 };
               };
-            };
-          }
-        ];
-      }
-      {
-        "Votre compte @gv.je unifié" = [
-          {
-            "Keycloak pour la connexion unifiée" = {
-              href = "https://key.gv.je/realms/master/account";
-              description = "Serveur de connexion et déconnexion unifiées (SSO en OAuth2) pour plusieurs services GV.";
-              icon = "si-keycloak";
-            };
-          }
-          {
-            "SFTP casiers numériques individuels" = {
-              href = "https://sftpgo.gv.je/";
-              icon = "mdi-folder-key-network";
-              noticon = "sh-sftpgo";
-              description = "Un espace pour ses fichiers et pour les partager.";
-            };
-          }
-          {
-            "Favoris (Linkding)" = {
-              description = "Partager des fichiers et des dossiers";
-              href = "https://linkding.lesgrandsvoisins.com/";
-              icon = "mdi-paperclip";
-            };
-          }
-          {
-            "HedgeDoc carnets collaboratifs en markdown" = {
-              href = "https://mark.lesgrandsvoisins.com/auth/oauth2";
-              noticon = "https://mark.lesgrandsvoisins.com/banner/banner_vertical_color.svg";
-              icon = "si-hedgedoc";
-              description = "Un serveur de documents en format Markdown pouvant être modifiés par plusieurs personnes en même temps.";
             };
           }
         ];
