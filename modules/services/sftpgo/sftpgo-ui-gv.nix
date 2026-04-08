82e1d1c17116c1e40887a3b1fad976ac8247a943
Chris Mann <chris@lesgtrandsvoisins.com>
2026-04-08 15:59:52 +0200

    webmin not working &

--- modules/services/sftpgo/sftpgo-ui-gv.nix
+++ modules/services/sftpgo/sftpgo-ui-gv.nix
@@ -16,6 +16,7 @@ pkgs.stdenv.mkDerivation {
       cp -a ${pkgs.sftpgo}/share/sftpgo/templates/common/base.html $out/templates/common/baselogin.html
       cp -a ${pkgs.sftpgo}/share/sftpgo/templates/webadmin/base.html $out/templates/webadmin/base.html
       cp -a ${pkgs.sftpgo}/share/sftpgo/templates/webadmin/base.html $out/templates/webadmin/baselogin.html
+      cp -a ${pkgs.sftpgo}/share/sftpgo/templates/webadmin/*.html $out/templates/webadmin/
     ''
     (pkgs.lib.strings.concatMapStrings (x: ''
         sed -i 's/<\/head>/<link type="text\/css" rel="stylesheet" href="https:\/\/public.gv.je\/static\/web\/gvbtn\/gvbtn.css">\n<\/head>/' ${x}
