d891bf847f497f74e2d7cc21167f29068c642699
Chris Mann <chris@lesgtrandsvoisins.com>
2026-04-18 09:13:24 +0200

    oxicloud

--- modules/services/oxicloud/package.nix
+++ modules/services/oxicloud/package.nix
@@ -19,12 +19,6 @@ pkgs.rustPlatform.buildRustPackage rec {
   # If tests fail due to DB, disable:
   doCheck = false;
 
-  cargoBuildFlags = [
-    "--release"
-    "--features"
-    "migrations"
-  ];
-
   # Install both binaries
   postInstall = ''
     mkdir -p $out/bin
@@ -32,11 +26,6 @@ pkgs.rustPlatform.buildRustPackage rec {
     # main server
     if [ -f target/release/oxicloud ]; then
       cp target/release/oxicloud $out/bin/
-    fi
-
-    # migration binary
-    if [ -f target/release/migrate ]; then
-      cp target/release/migrate $out/bin/oxicloud-migrate
     fi
 
     cp -r static $out/
