ab55bff2f808425091444a4baa3cdb99d3e1479d
Chris Mann <chris@lesgtrandsvoisins.com>
2026-04-19 21:46:17 +0200

    The package looks good

--- modules/services/sync-in/flake.nix
+++ modules/services/sync-in/flake.nix
@@ -2,7 +2,7 @@
   description = "Sync-in NixOS module + package";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = {
