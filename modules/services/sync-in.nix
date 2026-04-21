f3cffc94433e8c238bc6bf3921812020b89d2e61
Chris Mann <chris@lesgtrandsvoisins.com>
2026-04-21 13:48:32 +0200

    debug

--- modules/services/sync-in.nix
+++ modules/services/sync-in.nix
@@ -32,6 +32,7 @@ in {
     server = {
       port = vars.ports.sync-in;
       publicUrl = "https://sync-in.gv.je";
+      host = "0.0.0.0";
     };
     mail = {
       auth = {
@@ -87,13 +88,13 @@ in {
         issuerUrl = "https://key.gv.je/realms/master";
         clientId = "sync-in";
         clientSecretFile = "/etc/sync-in/oidc.secret";
-        redirectUri = "https://sync-in.gv.je/api/auth/oidc/callback";
+        redirectUri = "\"https://sync-in.gv.je/api/auth/oidc/callback\"";
         security.scope = "openid";
         options = {
           adminRoleOrGroup = "admin";
           autoCreatePermissions = ["personal_space" "spaces_access" "webdav_access"];
           autoRedirect = true;
-          buttonText = "key.gv.je";
+          buttonText = "\"key.gv.je\"";
           enablePasswordAuth = false;
         };
       };
