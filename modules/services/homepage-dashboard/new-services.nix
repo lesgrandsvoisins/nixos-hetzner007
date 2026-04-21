ff7b7bf1574cfc00a4cac43f641b5408542e7d68
Chris Mann <chris@lesgtrandsvoisins.com>
2026-04-21 10:53:06 +0200

    Cleaner and sync-in might even work now

--- modules/services/homepage-dashboard/new-services.nix
+++ modules/services/homepage-dashboard/new-services.nix
@@ -1,6 +1,13 @@
 {
   "PROFITEZ des nouveaux services des GV" = [
     {
+      "Vos CASIERS de fichiers via SYNC-IN" = {
+        href = "https://sync-in.gv.je";
+        icon = "si-sunrise";
+        description = "Vos fichiers avec votre casier SYNC-IN (Même fichiers que SFTPGo)";
+      };
+    }
+    {
       "Vos FICHIERS via casiers OXICLOUD" = {
         href = "https://oxicloud.gv.je";
         icon = "si-sunrise";
@@ -21,13 +28,13 @@
         icon = "si-forgejo";
       };
     }
-    {
-      "Vos PHOTOS via IMMICH" = {
-        # href = "https://immich.gv.je";
-        href = "https://memos.gv.je/memos/UhTemx3M2CkptqcJ9xighz";
-        icon = "si-immich";
-        description = "Vos photos et vidéos sur Immich";
-      };
-    }
+    # {
+    #   "Vos PHOTOS via IMMICH" = {
+    #     # href = "https://immich.gv.je";
+    #     href = "https://memos.gv.je/memos/UhTemx3M2CkptqcJ9xighz";
+    #     icon = "si-immich";
+    #     description = "Vos photos et vidéos sur Immich";
+    #   };
+    # }
   ];
 }
