281820bb4b2c6e3b7d618be032d0c0628e3f77d0
Chris Mann <chris@lesgtrandsvoisins.com>
2026-04-27 15:23:09 +0200

    Sync-In package correction.

--- modules/services/sync-in/package.nix
+++ modules/services/sync-in/package.nix
@@ -1,29 +1,36 @@
-{pkgs ? import <nixpkgs> {}}: let
+{
+  pkgs ? import <nixpkgs> {},
+  # lib,
+  # buildNpmPackage,
+  # fetchFromGitHub,
+  # runtimeShell,
+  # nodejs_24,
+  # nixosTests,
+}: let
   drizzleJsFile = ./drizzle.js;
   lib = pkgs.lib;
+  buildNpmPackage = pkgs.buildNpmPackage;
   fetchFromGitHub = pkgs.fetchFromGitHub;
   runtimeShell = pkgs.runtimeShell;
-  buildNpmPackage = pkgs.buildNpmPackage;
   nodejs_24 = pkgs.nodejs_24;
-  nixosTests = null;
 in
-  pkgs.buildNpmPackage {
+  buildNpmPackage rec {
     pname = "sync-in";
     version = "2.2.0";
 
-    src = pkgs.fetchFromGitHub {
+    src = fetchFromGitHub {
       owner = "Sync-in";
       repo = "server";
       rev = "v${version}";
       hash = "sha256-PkdVveWfAqEU/Ljs28OZt1QPjE+DqQX4Aet/wCk7eok=";
     };
 
-    nativeBuildInputs = [pkgs.nodejs_24];
+    nativeBuildInputs = [nodejs_24];
 
     postPatch = ''
-      ${pkgs.nodejs_24}/bin/npm pkg set dependencies.drizzle-orm="^0.45.2"
-      ${pkgs.nodejs_24}/bin/npm pkg set dependencies.pdfjs-dist="^5.6.205"
-      ${pkgs.nodejs_24}/bin/npm pkg set dependencies.drizzle-kit="^v0.31.10"
+      ${nodejs_24}/bin/npm pkg set dependencies.drizzle-orm="^0.45.2"
+      ${nodejs_24}/bin/npm pkg set dependencies.pdfjs-dist="^5.6.205"
+      ${nodejs_24}/bin/npm pkg set dependencies.drizzle-kit="^v0.31.10"
     '';
 
     # npmInstallFlags = "";
@@ -32,7 +39,9 @@ in
 
     buildPhase = ''
       runHook preBuild
-      ${pkgs.nodejs_24}/bin/npm run build && ${pkgs.nodejs_24}/bin/node scripts/build/release.mjs
+      ${nodejs_24}/bin/npm run build && ${nodejs_24}/bin/node scripts/build/release.mjs
+
+
       runHook postBuild
     '';
 
@@ -44,32 +53,18 @@ in
 
       mkdir -p $out/bin
       mkdir -p $out/conf
-      # cp $ {./drizzle.js} $out/conf/drizzle.js
-
-      cat > $out/conf/drizzle.js <<EOF
-      import { defineConfig } from "drizzle-kit";
-
-      export default defineConfig({
-        dialect: "mysql",
-        schema: "$out/lib/release/sync-in-server/server/infrastructure/database/schema.js",
-        out: "./backend-migrations",
-        url: "mysql://__USER__:__PASSWORD__@__HOST__:__PORT__/__NAME__",
-        tablesFilter: [
-          'files_content_*'
-        ]
-      });
-      EOF
+      cp ${drizzleJsFile} $out/conf/drizzle.js
 
       cat > $out/bin/sync-in <<EOF
-      #!${pkgs.runtimeShell}
+      #!${runtimeShell}
       export NODE_PATH=$NODE_PATH:$out/lib/node_modules
-      exec ${pkgs.nodejs_24}/bin/node $out/lib/release/sync-in-server/server/main.js "\$@"
+      exec ${nodejs_24}/bin/node $out/lib/release/sync-in-server/server/main.js "\$@"
       EOF
 
       cat > $out/bin/sync-in-migrate-db <<EOF
-      #!${pkgs.runtimeShell}
+      #!${runtimeShell}
       export NODE_PATH=$NODE_PATH:$out/lib/node_modules
-      exec ${pkgs.nodejs_24}/bin/npx drizzle-kit migrate --config=/etc/sync-in/drizzle.js "\$@"
+      exec ${nodejs_24}/bin/npx drizzle-kit migrate --config=/etc/sync-in/drizzle.js "\$@"
       EOF
 
       chmod +x $out/bin/sync-in
@@ -82,6 +77,7 @@ in
       description = "Sync-in server (self-hosted collaboration platform)";
       license = licenses.agpl3Only;
       platforms = platforms.linux;
+
       homepage = "https://github.com/Sync-in/server";
       changelog = "https://github.com/Sync-in/server/releases/tag/v${version}";
     };
