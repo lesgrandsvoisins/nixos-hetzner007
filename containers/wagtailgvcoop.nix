7888d98487eec952d1076ba006fbc68061bcc5b9
Chris Mann <chris@lesgtrandsvoisins.com>
2026-09-02 18:46:27 +0200

    caddy front

--- containers/wagtailgvcoop.nix
+++ containers/wagtailgvcoop.nix
@@ -5,108 +5,106 @@
   vars,
   ...
 }: let
-  nginxLocationWagtailExtraConfig = ''
-    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_redirect off;
-    proxy_http_version 1.1;
-    proxy_set_header X-Forwarded-Proto $scheme;
-    # proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    # proxy_set_header Host $host;
-    # proxy_set_header Upgrade $http_upgrade;
-    # proxy_set_header Connection $connection_upgrade_keepalive;
-  '';
-  nginxLesGrandsVoisinsExtraConfig = ''
-    # proxy_protocol off;
-    if ($host = 'meet.resdigita.com') {
-      return 302 https://jitsi.grandzine.org/resdigita;
-    }
-    if ($host = 'www.gdvoisins.org') {
-      return 302 https://www.gdvoisins.com$request_uri;
-    }
-    if ($host = 'www.lesgrandsvoisins.fr') {
-      return 302 https://www.lesgrandsvoisins.com$request_uri;
-    }
-    # Static assets: cache for a year (with versioned filenames)
-    location ~* \.(?:css|js|woff2?|ttf|eot|ico|gif|jpg|jpeg|png|webp|svg)$ {
-        expires 1w;
-        add_header Cache-Control "public, max-age=31536000, immutable";
-    }
-
-    # HTML: cache very briefly (optional)
-    location ~* \.(?:html)$ {
-        expires 5m;
-        add_header Cache-Control "public, max-age=300, must-revalidate";
-    }
-
-    # # API responses: no caching
-    # location /api/ {
-    #     add_header Cache-Control "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0";
-    #     proxy_pass http://localhost:8080;
-    # }
-
-    # Optionally disable ETag if you rely on versioned files
-    etag off;
-  '';
-  nginxLesGrandsVoisinsLocations = {
-    "/" = {
-      proxyPass = "http://localhost:${vars.ports.wagtailgvcoop}/";
-      extraConfig = ''
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_redirect off;
-        proxy_http_version 1.1;
-        proxy_set_header X-Forwarded-Proto $scheme;
-
-        # add_header Cache-Control "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0";
-
-        expires 5m;
-        add_header Cache-Control "public, max-age=300, must-revalidate";
-
-        # proxy_set_header Host $host;
-        # proxy_set_header Upgrade $http_upgrade;
-        # proxy_set_header Connection $connection_upgrade_keepalive;
-        # return 302 $scheme://www.grandsvoisins.com$request_uri;
-
-        rewrite ^/cms-admin/login/?$ /accounts/oidc/key-gv-je/login/?process=cms-admin/login/ redirect;
-      '';
-    };
-    "/fr/accounts/profile/".extraConfig = ''
-      return 302 /;
-    '';
-    "/en/accounts/profile/".extraConfig = ''
-      return 302 /;
-    '';
-    "/favicon.ico" = {proxyPass = null;};
-    "/static" = {proxyPass = null;};
-    "/media" = {proxyPass = null;};
-    "/medias" = {proxyPass = null;};
-    "/.well-known" = {proxyPass = null;};
-    "/index.php" = {
-      extraConfig = ''
-        return 404;
-      '';
-    };
-  };
+  # nginxLocationWagtailExtraConfig = ''
+  #   proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+  #   proxy_redirect off;
+  #   proxy_http_version 1.1;
+  #   proxy_set_header X-Forwarded-Proto $scheme;
+  #   # proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+  #   # proxy_set_header Host $host;
+  #   # proxy_set_header Upgrade $http_upgrade;
+  #   # proxy_set_header Connection $connection_upgrade_keepalive;
+  # '';
+  # nginxLesGrandsVoisinsExtraConfig = ''
+  #   # proxy_protocol off;
+  #   if ($host = 'meet.resdigita.com') {
+  #     return 302 https://jitsi.grandzine.org/resdigita;
+  #   }
+  #   if ($host = 'www.gdvoisins.org') {
+  #     return 302 https://www.gdvoisins.com$request_uri;
+  #   }
+  #   if ($host = 'www.lesgrandsvoisins.fr') {
+  #     return 302 https://www.lesgrandsvoisins.com$request_uri;
+  #   }
+  #   # Static assets: cache for a year (with versioned filenames)
+  #   location ~* \.(?:css|js|woff2?|ttf|eot|ico|gif|jpg|jpeg|png|webp|svg)$ {
+  #       expires 1w;
+  #       add_header Cache-Control "public, max-age=31536000, immutable";
+  #   }
+  #   # HTML: cache very briefly (optional)
+  #   location ~* \.(?:html)$ {
+  #       expires 5m;
+  #       add_header Cache-Control "public, max-age=300, must-revalidate";
+  #   }
+  #   # # API responses: no caching
+  #   # location /api/ {
+  #   #     add_header Cache-Control "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0";
+  #   #     proxy_pass http://localhost:8080;
+  #   # }
+  #   # Optionally disable ETag if you rely on versioned files
+  #   etag off;
+  # '';
+  # nginxLesGrandsVoisinsLocations = {
+  #   "/" = {
+  #     proxyPass = "http://localhost:${vars.ports.wagtailgvcoop}/";
+  #     extraConfig = ''
+  #       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+  #       proxy_redirect off;
+  #       proxy_http_version 1.1;
+  #       proxy_set_header X-Forwarded-Proto $scheme;
+  #       # add_header Cache-Control "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0";
+  #       expires 5m;
+  #       add_header Cache-Control "public, max-age=300, must-revalidate";
+  #       # proxy_set_header Host $host;
+  #       # proxy_set_header Upgrade $http_upgrade;
+  #       # proxy_set_header Connection $connection_upgrade_keepalive;
+  #       # return 302 $scheme://www.grandsvoisins.com$request_uri;
+  #       rewrite ^/cms-admin/login/?$ /accounts/oidc/key-gv-je/login/?process=cms-admin/login/ redirect;
+  #     '';
+  #   };
+  #   "/fr/accounts/profile/".extraConfig = ''
+  #     return 302 /;
+  #   '';
+  #   "/en/accounts/profile/".extraConfig = ''
+  #     return 302 /;
+  #   '';
+  #   "/favicon.ico" = {proxyPass = null;};
+  #   "/static" = {proxyPass = null;};
+  #   "/media" = {proxyPass = null;};
+  #   "/medias" = {proxyPass = null;};
+  #   "/.well-known" = {proxyPass = null;};
+  #   "/index.php" = {
+  #     extraConfig = ''
+  #       return 404;
+  #     '';
+  #   };
+  # };
 in {
-  services.nginx.virtualHosts = {
-    "www.gvcoop.org" = {
-      enableACME = true;
-      forceSSL = true;
-      root = "/var/www/wagtailgvcoop";
-      locations."/" = {
-        extraConfig = nginxLocationWagtailExtraConfig;
-        proxyPass = "http://wagtailgvcoop.containers:${builtins.toString vars.ports.wagtailgvcoop}/";
-      };
-      locations."/favicon.ico" = {proxyPass = null;};
-      locations."/static" = {proxyPass = null;};
-      locations."/medias" = {proxyPass = null;};
-      locations."/.well-known" = {proxyPass = null;};
-      locations."/fr/accounts/profile/".extraConfig = ''
-        return 302 /;
-      '';
-      locations."/en/accounts/profile/".extraConfig = ''
-        return 302 /;
-      '';
-    };
+  # services.nginx.virtualHosts = {
+  #   "www.gvcoop.org" = {
+  #     enableACME = true;
+  #     forceSSL = true;
+  #     root = "/var/www/wagtailgvcoop";
+  #     locations."/" = {
+  #       extraConfig = nginxLocationWagtailExtraConfig;
+  #       proxyPass = "http://wagtailgvcoop.containers:${builtins.toString vars.ports.wagtailgvcoop}/";
+  #     };
+  #     locations."/favicon.ico" = {proxyPass = null;};
+  #     locations."/static" = {proxyPass = null;};
+  #     locations."/medias" = {proxyPass = null;};
+  #     locations."/.well-known" = {proxyPass = null;};
+  #     locations."/fr/accounts/profile/".extraConfig = ''
+  #       return 302 /;
+  #     '';
+  #     locations."/en/accounts/profile/".extraConfig = ''
+  #       return 302 /;
+  #     '';
+  #   };
+  # };
+  "www.gvcoop.org" = {
+    extraConfig = ''
+      reverse_proxy http://${vars.containers.wagtailgvcoop.localAddress}:${builtins.toString vars.ports.wagtailgvcoop}
+    '';
   };
   users = {
     users.wagtailgvcoop = {
