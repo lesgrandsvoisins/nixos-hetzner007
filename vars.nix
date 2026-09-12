f5e25628984db9d6a7407dbd7c3dcbb8bbb96c2a
Chris Mann <chris@lesgtrandsvoisins.com>
2026-09-12 08:59:34 +0200

    Fine tunning

--- vars.nix
+++ vars.nix
@@ -261,10 +261,10 @@
         #   hostPath = "/var/www/voisinter-django/static/";
         #   isReadOnly = false;
         # };
-        "/var/www/voisinter-django/media/" = {
-          hostPath = "/var/voisinter/voisinter/media/";
-          isReadOnly = false;
-        };
+        # "/var/www/voisinter-django/media/" = {
+        #   hostPath = "/var/voisinter/voisinter/media/";
+        #   isReadOnly = false;
+        # };
         "/var/www/voisinter-django/" = {
           hostPath = "/var/www/voisinter-django/";
           isReadOnly = false;
